3392f5c1d94fef754f4f88470326bde9773af9da
Yatin Yadav <ytn.ydv@gmail.com>
2023-12-23 17:34:41 +0530

    Update mountSSD120.sh

--- mountSSD120.sh
+++ mountSSD120.sh
@@ -10,7 +10,7 @@ mkdir $directory
 mount $drive $directory
 
 # Add the mount to the fstab file to ensure it mounts at boot
-echo "$drive $directory ext4 defaults 0 0" >> /etc/fstab
+echo "$drive $directory ext4 defaults 0 0" | sudo tee -a /etc/fstab
 
 # Change the permissions of the mounted drive for accessibility
 chmod -R 777 $directory
