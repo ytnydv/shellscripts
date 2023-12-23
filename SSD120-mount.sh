0c32903391f10e181387f4f1c95936b3fbfa7ae5
Yatin Yadav <ytn.ydv@gmail.com>
2023-12-23 17:39:28 +0530

    Create SSD120-mount.sh

---
+++ SSD120-mount.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Declare the drive and directory variables
+drive="/dev/sda1"
+directory="/mnt/SSD120"
+
+# Create the directory where you'd like to mount the drive
+mkdir $directory
+
+# Mount the drive to the directory
+mount $drive $directory
+
+# Add the mount to the fstab file to ensure it mounts at boot
+echo "$drive $directory ext4 defaults 0 0" | tee -a /etc/fstab
+
+# Change the permissions of the mounted drive for accessibility.
+chmod -R 777 $directory
