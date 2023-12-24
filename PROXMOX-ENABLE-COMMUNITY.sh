ee2cde218b6e164dedf853ce5f1416d6aad303d7
Yatin Yadav <ytn.ydv@gmail.com>
2023-12-24 12:46:01 +0530

    Update PROXMOX-ENABLE-COMMUNITY.sh

--- PROXMOX-ENABLE-COMMUNITY.sh
+++ PROXMOX-ENABLE-COMMUNITY.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 # Disable enterprise repository
-sed -i.bak "s/^deb/\\#deb/" /etc/apt/sources.list.d/pve-enterprise.list
+sed -i.bak "s/^deb/\\\\#deb/" /etc/apt/sources.list.d/pve-enterprise.list
 
 # Disable ceph repository
-sed -i.bak "s/^deb/\\#deb/" /etc/apt/sources.list.d/ceph.list
+sed -i.bak "s/^deb/\\\\#deb/" /etc/apt/sources.list.d/ceph.list
 
 # Enable community repository
-echo "deb <http://download.proxmox.com/debian/pve> buster pve-no-subscription" > /etc/apt/sources.list.d/pve-install-repo.list
-echo "deb <http://download.proxmox.com/debian/pve> buster pve-no-subscription" > /etc/apt/sources.list.d/pve-no-enterprise.list
+echo "deb <http://download.proxmox.com/debian/pve> bookworm pve-no-subscription" > /etc/apt/sources.list.d/pve-install-repo.list
+echo "deb <http://download.proxmox.com/debian/pve> bookworm pve-no-subscription" > /etc/apt/sources.list.d/pve-no-enterprise.list
+
+# Enable security repository
+echo "deb <http://security.debian.org/debian-security> bookworm/updates main contrib" >> /etc/apt/sources.list
 
 # Update repositories
 apt-get update
+
+# Run upgrade
+apt-get upgrade -y
